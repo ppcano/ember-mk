62bf2176c0f383df111fe9d9b2c4b941a75549b8
Pepe Cano <ppcanodehuelva@gmail.com>
2012-01-27 13:18:39 +0100

    added scroll_wrapper

---
+++ packages/ember-mk/lib/mixins/scroll_wrapper.js
@@ -0,0 +1,43 @@
+
+require("ember-mk/core");
+
+var get = Ember.get , set = Ember.set, setPath = Ember.setPath, getPath = Ember.getPath;
+
+
+Mk.ScrollWrapper = Ember.Mixin.create({
+
+  heights: [],
+
+  didInsertElement: function() {
+    this._super();
+
+    var elementHeight 
+      , elementHeights = 0;
+
+    this.heights.forEach( function(item) {
+           
+     //   elementsHeight+= $('#'+item).height();
+      //
+      elementHeight = (typeof item === 'number')?item:$('#'+item).height();
+
+      ember_assert('scroll wrapper mixin (' + item + ') not found ', !!elementHeight);
+
+      elementHeights+=elementHeight;
+
+
+      //console.log('item ' + item + ' elementHeight ' + elementHeight + ' total ' + elementHeights  );
+
+    });
+
+
+
+    var wHeight = window.outerHeight;
+    var height = wHeight-elementHeights;
+    
+    //console.log( wHeight );
+    //console.log( elementHeights );
+    //console.log( height );
+    // scrolling
+    this.$().height(height); 
+  }
+});
